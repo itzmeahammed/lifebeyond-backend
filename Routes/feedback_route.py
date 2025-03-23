24cf145aa73759abe31b362f8db5d821ba59e2ce
naveenkumar <naveenkumar@wizzgeeks.com>
2025-03-23 15:51:14 +0530

    user sign up login and feedback

---
+++ Routes/feedback_route.py
@@ -0,0 +1,10 @@
+from Controllers.feedback_controller import FeedbackController
+from flask import Blueprint
+
+feedback_bp = Blueprint('Feedback', __name__)
+
+feedback_bp.add_url_rule('/getAllFeedback', view_func=FeedbackController.getAllFeedBack, methods=['POST'])
+feedback_bp.add_url_rule('/getFeedbackByUser', view_func=FeedbackController.getFeedbackByUser, methods=['POST'])
+feedback_bp.add_url_rule('/createFeedback', view_func=FeedbackController.createFeedback, methods=['POST'])
+feedback_bp.add_url_rule('/updateFeedback', view_func=FeedbackController.updateFeedback, methods=['PUT'])
+feedback_bp.add_url_rule('/deleteFeedback', view_func=FeedbackController.deleteFeedback, methods=['DELETE'])
